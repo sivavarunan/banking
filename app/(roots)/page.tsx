d1c1f2eebe3ad72b3e0848f428b98b168881cf95
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-10-11 22:19:29 +0530

    created HeaderBox component

--- app/(roots)/page.tsx
+++ app/(roots)/page.tsx
@@ -1,8 +1,16 @@
+import HeaderBox from '@/components/ui/HeaderBox'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox/>
+            </header>
+
+        </div>
+    </section>
   )
 }
 
