920d018a2b6e57af223e8eea1d9b83b0feb544b5
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-10-11 23:00:17 +0530

    installed react-query-string and added totalbalancebox

--- app/(roots)/page.tsx
+++ app/(roots)/page.tsx
@@ -1,25 +1,34 @@
 import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
 
-const loggedIn = { firstname: 'Siva'}
+    const loggedIn = { firstname: 'Siva' }
 
-  return (
-    <section className='home'>
-        <div className='home-content'>
-            <header className='home-header'>
-                <HeaderBox
-                type="greeting"
-                title="Welcome"
-                user={loggedIn?.firstname || 'Guest'}
-                subtext="Access and manage your transection efficienty"
-                />
-            </header>
+    return (
+        <section className='home'>
+            <div className='home-content'>
+                <header className='home-header'>
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstname || 'Guest'}
+                        subtext="Access and manage your transection efficienty"
+                    />
+                    <TotalBalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.35}
 
-        </div>
-    </section>
-  )
+                    
+                    />
+
+                </header>
+
+            </div>
+        </section>
+    )
 }
 
 export default Home
