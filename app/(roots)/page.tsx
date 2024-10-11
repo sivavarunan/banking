527a9c7e47924a0667af0f6504225f9e341007cc
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-10-11 22:40:46 +0530

    added content to Header-box

--- app/(roots)/page.tsx
+++ app/(roots)/page.tsx
@@ -2,11 +2,19 @@ import HeaderBox from '@/components/ui/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+
+const loggedIn = { firstname: 'Siva'}
+
   return (
     <section className='home'>
         <div className='home-content'>
             <header className='home-header'>
-                <HeaderBox/>
+                <HeaderBox
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstname || 'Guest'}
+                subtext="Access and manage your transection efficienty"
+                />
             </header>
 
         </div>
