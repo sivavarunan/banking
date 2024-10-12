68e8032d1484d0ba7396c4b52a1f9aedac776016
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-10-12 11:31:42 +0530

    added chartjs package , error fix(hydration error p) added dougnutchart component

--- components/ui/TotalBalanceBox.tsx
+++ components/ui/TotalBalanceBox.tsx
@@ -1,11 +1,13 @@
 import { formatAmount } from '@/lib/utils'
 import React from 'react'
 import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({accounts = [],totalBanks, totalCurrentBalance}:TotlaBalanceBoxProps) => {
   return (
     <section className='total-balance'>
         <div className='total-balance-chart'>
+            <DoughnutChart accounts={accounts} />
         </div>
         <div className='flex flex-col gap-6'>
             <h2 className='header-2'>
@@ -15,11 +17,11 @@ const TotalBalanceBox = ({accounts = [],totalBanks, totalCurrentBalance}:TotlaBa
                 <p className='total-balance-label'>
                     Total current balance
                 </p>
-                <p className='total-balance-amount flex-center gap-2'>
+                <div className='total-balance-amount flex-center gap-2'>
                     <AnimatedCounter
                     amount={totalCurrentBalance}
                     /> 
-                </p>
+                </div>
             </div>
         </div>
         </section>
