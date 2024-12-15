5240825d2fc179c472a9ab654c6e150636eaefd3
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-12-15 22:46:32 +0530

    ...

--- app/(roots)/my-banks/page.tsx
+++ app/(roots)/my-banks/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const MyBanks = () => {
   return (
-    <div>MyBanks</div>
+    <div>
+      <div className='home-content'>
+        My Banks
+      </div>
+    </div>
   )
 }
 
