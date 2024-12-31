c4c6a6926188d280444d23294dd9e2599b0fe318
sivavarunan <thevarasa.20221591@iit.ac.lk>
2024-12-31 14:19:38 +0530

    removed spinner in loading.tsx

--- components/ui/loading.tsx
+++ components/ui/loading.tsx
@@ -1,11 +1,7 @@
 const Loading = ({ message }: { message?: string }) => {
     return (
       <div className="flex flex-col items-center justify-center h-screen ">
-        {/* Spinner */}
-        <div className="relative w-16 h-16 mb-6">
-          <div className="absolute inset-0 border-4 border-blue-300 rounded-full border-t-transparent animate-spin"></div>
-        </div>
-  
+      
         {/* Progress Bar */}
         <div className="w-64 h-2 bg-blue-200 rounded-full overflow-hidden mb-4">
           <div className="h-full bg-blue-500 animate-progress-bar"></div>
